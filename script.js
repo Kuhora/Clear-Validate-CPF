59f88488c74c7b9b2ab1339b747c8d307f6060d2
Matheus Fragasse <kuhora@gmail.com>
2024-01-26 18:55:57 -0300

    Pt 2

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 const cpfs = document.querySelectorAll('.cpf li');
 
-function elementInnerText([...elements]) {
-  const arrayElements = Array.from(elements);
 
-  return arrayElements.map(element => {
-    return element.innerText;
-  });
+const elementInnerText = ([...elements]) => {
+
+  return elements.map(element => element.innerText);
 };
 
+const clearCPF = (cpf) => {
+  return cpf.replace(/\D/g, '');
+}
+
+const buildCPF = (cpf) => {
+  return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g, '$1.$2.$3-$4');
+}
+
+console.log(clearCPF('121.121.222 33'));
+console.log(buildCPF('12112122233'));
 console.log(elementInnerText(cpfs));
+
+
